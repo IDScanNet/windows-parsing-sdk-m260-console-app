595ac3ef4f9dc2f5352a4fd2bbdd2b7393a362a3
Cary Grant II <90705075+IDScanCaryGrantII@users.noreply.github.com>
2023-03-20 12:08:44 -0500

    Updating to be more clear

--- windows-parsing-sdk-m260-console-app/Program.cs
+++ windows-parsing-sdk-m260-console-app/Program.cs
@@ -1,4 +1,4 @@
-﻿using Nautilus;
+using Nautilus;
 using System;
 using System.IO;
 using System.IO.Ports;
@@ -13,7 +13,7 @@ public class PortChat
     public static void Main()
     {
         string message;
-        DriverLicense dl = new DriverLicense();
+        DriverLicense resDL = new DriverLicense();
 
         StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
 
@@ -39,7 +39,7 @@ public class PortChat
 
         file = new StreamWriter("drivers-license.txt", append: true);
 
-        Console.WriteLine("Type Parse to parse the ID");
+        Console.WriteLine("Scan your license first then type Parse to parse the ID");
 
         while (_continue)
         {
@@ -49,14 +49,59 @@ public class PortChat
             {
                 file.Close();
                 string text = System.IO.File.ReadAllText("drivers-license.txt");
-                dl.ExtractInfo(text);
+                resDL.ExtractInfo(text);
 
-                Console.Out.WriteLine(dl.FullName);
-                Console.Out.WriteLine(dl.FirstName);
-                Console.Out.WriteLine(dl.LastName);
-                Console.Out.WriteLine(dl.IssueDate);
-                Console.Out.WriteLine(dl.ExpirationDate);
+                Console.Out.WriteLine($"To learn about a specific field visit\nhttps://docs.idscan.net/idparsing/dotnet.html\n");
+                Console.WriteLine($"Address : {resDL.Address1} {resDL.Address2}");
+                Console.WriteLine($"\nDocument Type : {resDL.DocumentType}");
+                Console.WriteLine($"\nBirthday:{resDL.Birthdate}");
+                Console.WriteLine($"\nCity : {resDL.City}");
+                Console.WriteLine($"\nVehicle Classification Code : {resDL.VehicleClassCode}");
+                Console.WriteLine($"\nCompliance Type: {resDL.ComplianceType}");
+                Console.WriteLine($"\nCountry : {resDL.Country}");
+                //  Console.WriteLine($"\nDiscretionary ID Number : {resDL.DiscretionaryIdNumber}");
+                Console.WriteLine($"\nDocument Type : {resDL.DocumentType}");
+                Console.WriteLine($"\nDocument Discriminator : {resDL.DocumentDiscriminator}");
+                Console.WriteLine($"\nEndorsment Code Description : {resDL.EndorsementCodeDescription}");
+                Console.WriteLine($"\nEndorsements Code : {resDL.EndorsementsCode}");
+                Console.WriteLine($"\nExpiration Date : {resDL.ExpirationDate}");
+                Console.WriteLine($"\nEye Color : {resDL.EyeColor}");
+                Console.WriteLine($"\nFirst Name : {resDL.FirstName}");
+                Console.WriteLine($"\nFull Name : {resDL.FullName}");
+                Console.WriteLine($"\nGender : {resDL.Gender}");
+                Console.WriteLine($"\nHair Color : {resDL.HairColor}");
+                Console.WriteLine($"\nHazmat Exp Date : {resDL.HAZMATExpDate}");
+                Console.WriteLine($"\nHeight : {resDL.Height}");
+                Console.WriteLine($"\nIIN : {resDL.IIN}");
+                Console.WriteLine($"\nIssueDate : {resDL.EndorsementsCode}");
+                Console.WriteLine($"\nJurisdiction Code : {resDL.JurisdictionCode}");
+                Console.WriteLine($"\nLast Name : {resDL.LastName}");
+                Console.WriteLine($"\nLicense Number : {resDL.LicenseNumber}");
+                Console.WriteLine($"\nLimited Duration Document : {resDL.LimitedDurationDocument}");
+                Console.WriteLine($"\nMiddle Name : {resDL.MiddleName}");
+                Console.WriteLine($"\nName Prefix : {resDL.NamePrefix}");
+                Console.WriteLine($"\nName Suffix : {resDL.NameSuffix}");
+                Console.WriteLine($"\nOrgan Donor : {resDL.OrganDonor}");
+                Console.WriteLine($"\nPostal Box : {resDL.PostalBox}");
+                Console.WriteLine($"\nPostal Code : {resDL.PostalCode}");
 
+                Console.WriteLine($"\nRace : {resDL.Race}");
+                Console.WriteLine($"\nRealId  : {resDL.RealId}");
+                Console.WriteLine($"\nRestriction Code : {resDL.RestrictionCode}");
+                Console.WriteLine($"\nRestriction Code Description : {resDL.RestrictionCodeDescription}");
+                Console.WriteLine($"\nSpecification : {resDL.Specification}");
+                Console.WriteLine($"\nVehicle Class Code Description : {resDL.VehicleClassCodeDescription}");
+                Console.WriteLine($"\nVechile Registration Data : {resDL.vehicleRegistrationData}");
+                Console.WriteLine($"\nVehicle Class Code  : {resDL.VehicleClassCode}");
+                Console.WriteLine($"\nValidation Conf : {resDL.ValidationConfidence}");
+                foreach (string s in resDL.ValidationCodes)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine($"\nWeight KG : {resDL.WeightKG}");
+                Console.WriteLine($"\nWeight LBS : {resDL.WeightLBS}");
+
+                Console.Out.Write("To use this sample again, please clear/delete driver-license.txt file");
 
 
             }
@@ -99,4 +144,4 @@ public class PortChat
            
         }
     }
-}
+}  
